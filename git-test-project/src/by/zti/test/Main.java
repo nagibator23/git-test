92649fe719469ba58a1a0623f9d960ee2f2b7dca
Андрей <@192.168.100.5>
2017-04-29 19:42:09 +0300

    ggg

--- git-test-project/src/by/zti/test/Main.java
+++ git-test-project/src/by/zti/test/Main.java
@@ -2,6 +2,6 @@ package by.zti.test;
 
 public class Main {
 public static void main (String []args){
-	System.out.println("Hello from git  !!!!!");
+	System.out.println("Hello from git  fdsfdsfdsfdsfds!!!!!");
 }
 }
